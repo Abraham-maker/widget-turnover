61e0c47f6abd55e4ed82c80c4c958398c7cc65b8
Abraham-maker <imnoteverywhere12@gmail.com>
2022-08-24 08:24:24 -0400

    register

--- src/components/Views/Auth/ForgotPassword.jsx
+++ src/components/Views/Auth/ForgotPassword.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import './Styles/ForgotPassword.css'
 
 const ForgotPassword = () => {
+  const { push } = useHistory();
   return (
     <div className='container-forgot'>
       <img src="https://www.turnover.gotopdev.com/assets/images/LogoAzul.png" alt="turnover" />
@@ -9,6 +11,7 @@ const ForgotPassword = () => {
         <input type="text" placeholder='Introduce tu e-mail' />
         <button className='btn-forgot'><span className='span-forgot'>Recuperar contraseña</span></button>
       </div>
+      <p className='view-forgot__password' onClick={() => { push('/') }}>Login</p>
     </div>
   )
 }
--- src/components/Views/Auth/Register.jsx
+++ src/components/Views/Auth/Register.jsx
@@ -1,8 +1,38 @@
 import React from 'react'
+import './Styles/Register.css'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <>
+      <div className='container__register'>
+        <img src="https://www.turnover.gotopdev.com/assets/images/LogoAzul.png" alt="turnover" />
+        <div className="form-regiter">
+          <input type="email" className='input-email' placeholder='E-mail' />
+
+          <div className='group-input'>
+            <input type="password" className='input-password' placeholder='Contraseña' />
+            <input type="password" className='input-password' placeholder='Confirmar contraseña' />
+          </div>
+
+          <div className='group-input'>
+            <input type="text" className='input-name' placeholder='Nombre' />
+            <input type="text" className='input-name' placeholder='Apellidos' />
+          </div>
+
+          <input type="text" className='input-email' placeholder='Dirección' />
+
+          <div className='group-input'>
+            <input type="text" className='input-name' placeholder='Codigo postal' />
+            <input type="text" className='input-name' placeholder='Ciudad' />
+          </div>
+          
+          <input type="checkbox" id="fruit1" name="fruit-1" value="Apple" />
+          <label for="fruit1">Estoy de acuerdo con los <span className='terminos'>Términos y Condiciones</span></label>
+
+          <button className='btn-crear__cuenta'><span className='span-crear__cuenta'>Crear cuenta</span></button>
+        </div>
+      </div>
+    </>
   )
 }
 
