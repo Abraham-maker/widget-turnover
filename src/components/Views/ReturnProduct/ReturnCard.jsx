747423614c9199e3aae9c502cebe34a2097c20f0
Abraham-maker <imnoteverywhere12@gmail.com>
2022-09-11 23:35:21 -0400

    card

--- src/components/Views/ReturnProduct/ReturnCard.jsx
+++ src/components/Views/ReturnProduct/ReturnCard.jsx
@@ -9,24 +9,56 @@ const ReturnCard = ({ setOpenModal }) => {
     const { push } = useHistory()
     const [open, setOpen] = useState(false);
     const [datesCard, setDatesCard] = useState({});
-
+    const [errors, setErrors] = useState({})
+    const { date, num, type, name, address, value } = errors ?? false;
+    const [actualState, changeCheckState] = useState(false);
+    const errorDate = (date ?? [])[0];
+    const errorNum = (num ?? [])[0];
+    const errorName = (name ?? [])[0];
+    const errorAddrees = (address ?? [])[0];
+    const errorValue = (value ?? [])[0];
+    const errorTypt = (type ?? [])[0];
     const closeModal = () => {
         window.localStorage.removeItem('InfoLogin', true)
         push('/')
         setOpenModal(false)
     }
 
+    const handleChexbox = (e) => {
+        changeCheckState(e.target.checked);
+    }
+
     const getDates = ({ target }) => {
         const { name, value } = target;
         setDatesCard({ ...datesCard, [name]: value })
     }
+    console.log(datesCard.type);
 
-    const getCard = () => {
+    const validateCard = () => {
         setLoading(true);
-        setTimeout(() => {
-            setLoading(false)
-            push('/view-repayment')
-        }, 1000);
+        const headers = {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        };
+
+        let body = {
+            terms: actualState,
+            name: datesCard.name,
+            value: datesCard.value,
+            address: datesCard.address,
+            date: datesCard.date,
+            num: datesCard.num,
+            type: datesCard.type
+        }
+        fetch("https://www.turnover.gotopdev.com/api/v1/validatedck", { headers, method: "POST", body: JSON.stringify(body) })
+            .then(res => res.json())
+            .then((errors) => {
+                setLoading(false)
+                setErrors(errors.errors)
+                if (errors.status === 'Success') {
+                    push('/view-repayment')
+                }
+            })
     }
 
     return (
@@ -55,32 +87,50 @@ const ReturnCard = ({ setOpenModal }) => {
                     <p>Información de la tarjeta</p>
 
                     <div id="div-inputs__card">
-                        <input type="text" maxLength={25} id='card_number' placeholder='Número de tarjeta bancaria' name='creditCard' onChange={getDates} />
-                        <select id='select-card' onChange={getDates} name='tarjeta'>
-                            <option selected disabled>Selecciona tu tarjeta</option>
-                            <option value="Visa">Visa</option>
-                            <option value="Master Card">Master Card</option>
-                            <option value="Platino">Platino</option>
-                        </select>
-                        <input type="text" maxLength={10} id='titular' placeholder='Titular de la tarjeta' name='type_creditCard' onChange={getDates} />
-                        <input type="text" maxLength={25} id='address_card' placeholder='Dirección' name='direccion_card' onChange={getDates} />
+                        <div>
+                            <input type="text" maxLength={25} id='card_number' placeholder='Número de tarjeta bancaria' name='value' onChange={getDates} />
+                            {errorValue ? (<><div className='message-error'>{errorValue}</div></>) : false}
+                        </div>
+                        <div>
+                            <select id='select-card' onChange={getDates} name='type'>
+                                <option selected disabled>Selecciona tu tarjeta</option>
+                                <option value="1">Visa</option>
+                                <option value="1">Master Card</option>
+                                <option value="1">Platino</option>
+                            </select>
+                            {errorTypt ? (<><div className='message-error'>{errorTypt}</div></>) : false}
+                        </div>
+                        <div>
+                            <input type="text" maxLength={20} id='titular' placeholder='Titular de la tarjeta' name='name' onChange={getDates} />
+                            {errorName ? (<><div className='message-error'>{errorName}</div></>) : false}
+                        </div>
+                        <div>
+                            <input type="text" maxLength={25} id='address_card' placeholder='Dirección' name='address' onChange={getDates} />
+                            {errorAddrees ? (<><div className='message-error'>{errorAddrees}</div></>) : false}
+                        </div>
                         <div className='div-flex'>
-                            <input type='date' id='expirado' placeholder='Fecha de vencimiento' name='fecha_expirada' onChange={getDates} />
-                            <input type="text" maxLength={4} id='cvv' placeholder='CVV' name='cvv' onChange={getDates} />
+                            <div>
+                                <input type='text' id='expirado' placeholder='Fecha de vencimiento' name='date' onChange={getDates} />
+                                {errorDate ? (<><div className='message-error'>{errorDate}</div></>) : false}
+                            </div>
+                            <div>
+                                <input type="text" maxLength={4} id='cvv' placeholder='CVV' name='num' onChange={getDates} />
+                                {errorNum ? (<><div className='message-error'>{errorNum}</div></>) : false}
+                            </div>
                         </div>
                     </div>
 
-                    <input type="checkbox" id="term-card" name="terms" onChange={getDates} />
+                    <input type="checkbox" id="term-card" name="terms" onChange={handleChexbox} />
                     <label htmlFor="term-card"> He leído, Entiendo y acepto  <span id='terminos-card'> Condiciones de reembolso</span> de TurnOver.</label>
                 </div>
 
-                {datesCard.creditCard === undefined || datesCard.creditCard.length === 0 || datesCard.tarjeta === undefined || datesCard.tarjeta.length === 0 || datesCard.type_creditCard === undefined || datesCard.type_creditCard.length === 0 || datesCard.fecha_expirada === undefined || datesCard.fecha_expirada.length === 0 || datesCard.cvv === undefined || datesCard.cvv.length === 0 ?
+                {datesCard.value === undefined || datesCard.value.length === 0 || datesCard.name === undefined || datesCard.name.length === 0 || datesCard.address === undefined || datesCard.address.length === 0 || datesCard.date === undefined || datesCard.date.length === 0 || datesCard.num === undefined || datesCard.num.length === 0 || actualState === false ?
                     (<>
                         <button id='bt-card'>Confirmar reembolso</button>
                     </>) :
                     (<>
+                        <button id='bt-card-active' onClick={validateCard}>Confirmar reembolso</button>
                         {!!loading ? (<><div className='spinner'></div></>) : false}
-                        <button id='bt-card-active' onClick={getCard}>Confirmar reembolso</button>
                     </>)}
 
             </div>
